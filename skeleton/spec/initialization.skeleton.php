2c80da997dbea01edbcdc633173eb2f6dff8a4a8
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2010-01-12 02:16:22 +0000

    git-svn-id: http://samurai-fw.org/source/svn/trunk@13 04c1ff95-0a9c-40b3-b2d5-9109686ccee5

--- skeleton/spec/initialization.skeleton.php
+++ skeleton/spec/initialization.skeleton.php
@@ -13,7 +13,8 @@
 //AG設定
 Samurai_Loader::load('library/ActiveGateway/ActiveGatewayManager.class.php');
 $AGManager = ActiveGatewayManager::singleton();
-$AGManager->import(Samurai_Loader::getPath('config/activegateway/activegateway.<?=$project_name?>.yml'));
+$AGManager->import(Samurai_Loader::getPath('config/activegateway/activegateway.yml'));
+$AGManager->import(Samurai_Loader::getPath('config/activegateway/activegateway.production.yml'));
 //$AGManager->import(Samurai_Loader::getPath('config/activegateway/activegateway.development.yml'));
 //$AGManager->import(Samurai_Loader::getPath('config/activegateway/activegateway.sandbox.yml'));
 $AG = $AGManager->getActiveGateway('sandbox');
