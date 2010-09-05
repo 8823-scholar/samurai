7ffd7d5c5580245de61aa59c1de72d8c5db0e87f
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2010-09-05 19:34:44 +0000

    URI中にPREFIXと同じ文字列があると、Routingが正常に動作しないバグを修正
    git-svn-id: http://samurai-fw.org/source/svn/trunk@52 04c1ff95-0a9c-40b3-b2d5-9109686ccee5

--- config/environment/production.php
+++ config/environment/production.php
@@ -48,7 +48,7 @@
 if(isset($_SERVER['HTTP_HOST'])){
     defined('BASE_URI') ? NULL : define('BASE_URI', dirname($_SERVER['SCRIPT_NAME']) == '/' ? '' : dirname($_SERVER['SCRIPT_NAME'])) ;
     defined('BASE_URL') ? NULL : define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . BASE_URI) ;
-    defined('MAIN_URI') ? NULL : define('MAIN_URI', preg_replace('/'.preg_quote(BASE_URI, '/').'/', '', $_SERVER['REQUEST_URI'])) ;
+    defined('MAIN_URI') ? NULL : define('MAIN_URI', preg_replace('/^'.preg_quote(BASE_URI, '/').'/', '', $_SERVER['REQUEST_URI'])) ;
     Samurai_Config::set('url.base_uri', BASE_URI);
     Samurai_Config::set('url.base_url', BASE_URL);
     Samurai_Config::set('url.main_uri', MAIN_URI);
